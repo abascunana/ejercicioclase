40aade914e6dd31b7ee56a58d41a9539513396ba
abascunana <91748309+abascunana@users.noreply.github.com>
2021-11-13 14:11:13 +0100

    Update cambio.py

--- cambio.py
+++ cambio.py
@@ -4,14 +4,18 @@ bil = int(input("introduce la cantidad que quiera devolver en cambio "))
 #en esta lista se registran los resultados de cambio de la máquina
 y=[]
 #bucle que revisa 7 veces el resultado de dividir el cambio pedido por todas la opciones disponibles
-for i in range (0,7):
+i= 0
+for i in range (ma[i]):
   a=int(bil/ma[i])
   #condicional que revisa si el resultado de "y" sea mayor
   # a 0 y que de una cantidad de cambio buena, si se cumple, se registra en "y"
+  
   if a > 0 and (bil%ma[i])==0:
      y.append(a)
 # condicional que cambia entre el plural y singular
+    
      if min(y) == 1:
        print(i+1,"opcion la máquina te devuelve",min(y),"billete de",ma[i],"euros")       
      else:
        print(i+1,"opcion la máquina te devuelve",min(y),"billetes de",ma[i],"euros")
+i +=1
