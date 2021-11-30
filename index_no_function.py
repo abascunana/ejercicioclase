951cccd2ab5e686ea6344873c8aa4b1236d93627
abascunana <91748309+abascunana@users.noreply.github.com>
2021-11-30 17:33:46 +0100

    Update index_no_function.py
    lista excluida de la función

--- index_no_function.py
+++ index_no_function.py
@@ -1,9 +1,10 @@
-def indexse(palabra): #esta función hace lo mismo que el index()
-    x = ["hola","holo","colo","84"]
+def indexse(palabra,x): #esta función hace lo mismo que el index()
+    
     for i in range(len(x)): # bucle que revisa la largaria de lista x
-        if x[i] == palabra: #si lista x(se tiene que cambiar) contiene el elemento "palabra",printea la posción
-           print("elemento localizado en posición",i)
-        else:
-            print("no localizado")
-    print(x) #print x para comprobar
-indexse("83")
+        if x[i] in palabra: #si lista x(se tiene que cambiar) contiene el elemento "palabra",printea la posción
+           a = ("elemento localizado en posición",i)
+    if x[i] not in palabra:
+        a = "no localizado"
+    return(a) #print x para comprobar
+
+print(indexse("84",["8","82","84"]))
